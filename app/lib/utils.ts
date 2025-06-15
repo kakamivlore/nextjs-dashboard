7026f08efd05d5ee32a4ec4e1eafc0debb496d0f
kakamivlore <kakamivlore71@gmail.com>
2025-06-15 18:52:43 +0200

    to chapter 11

--- app/lib/utils.ts
+++ app/lib/utils.ts
@@ -3,13 +3,13 @@ import { Revenue } from './definitions';
 export const formatCurrency = (amount: number) => {
   return (amount / 100).toLocaleString('en-US', {
     style: 'currency',
-    currency: 'USD',
+    currency: 'EUR',
   });
 };
 
 export const formatDateToLocal = (
   dateStr: string,
-  locale: string = 'en-US',
+  locale: string = 'it-IT',
 ) => {
   const date = new Date(dateStr);
   const options: Intl.DateTimeFormatOptions = {
@@ -29,7 +29,7 @@ export const generateYAxis = (revenue: Revenue[]) => {
   const topLabel = Math.ceil(highestRecord / 1000) * 1000;
 
   for (let i = topLabel; i >= 0; i -= 1000) {
-    yAxisLabels.push(`$${i / 1000}K`);
+    yAxisLabels.push(`€${i / 1000}K`);
   }
 
   return { yAxisLabels, topLabel };
